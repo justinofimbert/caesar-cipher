dbc12bb1b06e26481b3652466798f6c545196947
justinofimbert <justinofimbert@gmail.com>
2022-04-12 02:53:29 -0300

    Refactor Player class with new markers class method

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -1,19 +1,26 @@
 class Player
   attr_reader :name, :marker
+
+  @@markers = []
+
   def initialize
     @name = gets.chomp.strip
+    Game.scores[self.name] = 0
   end
 
   def set_marker
     until @marker
       user_input = gets.chomp.strip
       if user_input.length == 1
-        self.marker = user_input
+        @marker = user_input
+        @@markers.push(user_input)
       end
     end
   end
 
-
+  def self.markers
+    @@markers
+  end
 end
 
 class Match
