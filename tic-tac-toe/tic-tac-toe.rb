c8b1434b2666a2cb0dc349adeba1582710e26e97
justinofimbert <justinofimbert@gmail.com>
2022-04-13 20:42:02 -0300

    Refactor entire project to follow style conventions

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -1,7 +1,7 @@
 class Game
   @@scores = {}
 
-  def winning_player(player)
+  def increase_score(player)
     @@scores[player.name] += 1
   end
 
@@ -17,7 +17,7 @@ class Player
 
   def initialize
     @name = gets.chomp.strip
-    Game.scores[self.name] = 0
+    Game.scores[name] = 0
   end
 
   def set_marker
@@ -39,58 +39,58 @@ class Board
   attr_accessor :board
 
   def initialize
-    @board = Array.new(3) {Array.new(3, nil)}
+    @board = Array.new(3) { Array.new(3, nil) }
   end
 
   def show
-    self.board.each { |row| p row}
+    board.each { |row| p row }
   end
 
   def place_choice(player)
-
-    # until player_choice is an int and is between 1 and 9 get their input
-    until player_choice do
-      user_choice = gets.chomp.strip.to_i
-      if user_choice.between? && user_choice.is_a?(Integer) && Player.markers.none?(self.board.flatten[user_choice-1])
+    # until player_choice is an int and is between 1 and 9 get their input and is a free space
+    until player_choice
+      user_choice = gets.chomp.strip.to_i - 1
+      if user_choice.between?(0, 8) && user_choice.is_a?(Integer) && !Player.markers.include?(board.flatten[user_choice])
         player_choice = user_choice
-        break
       end
     end
 
     player_choice -= 1
     # set player marker to its corresponding position
-    self.board[player_choice / 3][player_choice % 3] = player.marker
+    board[player_choice / 3][player_choice % 3] = player.marker
   end
 
   def check_state(player)
-
-    checks = Hash.new
-    
+    checks = {}
     # check in each row if an specific player has won
-    checks[:row] = self.board.any? do |row|
+    checks[:row] = board.any? do |row|
       return true if row.unique.first == player.marker && row.unique.length == 1
+
       false
     end
 
-    #check in each column if an specific player has won
+    # check in each column if an specific player has won
     checks[:column] = 3.times do |column|
-      return true if self.board[column] == player.marker && self.board.flatten[column..[6+column]].step(2).unique.length == 1
+      return true if board[0][column] == player.marker && board.flatten[column..[6 + column]].step(2).unique.length == 1
+
       false
     end
 
-    diagonal1 = self.board.flatten.step(3).unique.length == 1 && self.board[0][0] == player.marker
+    diagonal1 = board.flatten.step(3).unique.length == 1 && board[0][0] == player.marker
 
-    diagonal2 = self.board.flatten[2..6].step(1).unique.length == 1 && self.board[0][2] == player.marker
+    diagonal2 = board.flatten[2..6].step(1).unique.length == 1 && board[0][2] == player.marker
 
     checks[:diagonals] = diagonal1 || diagonal2
-    if checks.any? { |_key, value| value == true }
-      self.reset
-      true
-    end
+
+    return false unless checks.any? { |_key, value| value == true }
+
+    self.reset
+    true
   end
 
   private
+
   def reset
-    self.board = Array.new(3) {Array.new(3, nil)}
+    self.board = Array.new(3) { Array.new(3, nil) }
   end
 end
