0eb6a9588b1a8b35f9c826587167dd971c73c899
justinofimbert <justinofimbert@gmail.com>
2022-04-27 16:57:27 -0300

    Update to clean code and follow ruby conventions

--- mastermind/player.rb
+++ mastermind/player.rb
@@ -1,5 +1,6 @@
 class Player
   attr_reader :role, :is_user
+
   def initialize(role, is_user)
     @role = role
     @is_user = is_user
@@ -10,13 +11,13 @@ class Player
   end
 
   def create_secret_code
-    return nil if self.role != codemaker 
-    
+    return nil if role != codemaker
+
     if is_user? == false
       secret_code = []
 
-      4.times { secret_code.push(rand(8) + 1) }
-      return secret_code
+      4.times { secret_code.push(rand(1..8)) }
+      secret_code
 
     else
       secret_code = false
@@ -24,7 +25,7 @@ class Player
         Display.ask_user_secret_code
         user_input = gets.chomp
 
-        if user_input.length == 4 && user_input.split('').all { |value| (value.to_i).between(1, 8) }
+        if user_input.length == 4 && user_input.split('').all { |value| value.to_i.between(1, 8) }
           secret_code = user_input
 
         else
