648fc9c25e06d42f111f8482e7cee17e88eea423
justinofimbert <justinofimbert@gmail.com>
2022-04-27 18:51:00 -0300

    Create check_guess method in Board class

--- mastermind/board.rb
+++ mastermind/board.rb
@@ -1,5 +1,5 @@
 class Board
-  attr_reader :max_guesses, :secret_code
+  attr_reader :max_guesses, :secret_code, :guess_number
 
   def initialize(max_guesses)
     @max_guesses = max_guesses
@@ -7,5 +7,20 @@ class Board
 
   def set_secret_code=(secret_code)
     @secret_code = secret_code
+  end
+
+  def check_guess(guess)
+    return_message = ''
+    temp_code = @secret_code.split('')
+    guess.split('').each_with_index do |guess_digit, index|
+
+      if temp_code.include? guess_digit
+        return_message += guess_digit == temp_code[index] ? 'correct position' : 'incorrect position'
+
+      else
+        return_message += 'incorrect digit'
+    end
+
+    return_message
   end
 end
