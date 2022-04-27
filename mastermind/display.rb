512421681c4ee81afa5b3b164f5797404fb991d6
justinofimbert <justinofimbert@gmail.com>
2022-04-26 21:10:38 -0300

    Add ask_user_secret_code method to player.rb
    this function asks the codemaker to create a secret code that the other
    player will have to find, there are methods on the Display module that
    have been created to communicate with the user

--- mastermind/display.rb
+++ mastermind/display.rb
@@ -1,9 +1,17 @@
 module Display
   def ask_user_name
-    puts "Hello, how may I call you?"
+    puts 'Hello, how may I call you?'
   end
 
   def ask_user_role
     puts 'Do you want to be the codebreaker?'
+  end
+
+  def ask_user_secret_code
+    'You are the codemaker, you have to enter a 4-digit code where every one of its numbers is from 1 to 8'
+  end
+
+  def ask_user_errorless_code
+    puts 'Oops! Remember! you have to enter a 4-digit code composed just by numbers, where every one of them is different than 0 and 9'
   end
 end
--- mastermind/player.rb
+++ mastermind/player.rb
@@ -8,4 +8,29 @@ class Player
   def is_user?
     @is_user
   end
+
+  def create_secret_code
+    return nil if self.role != codemaker 
+    
+    if is_user? == false
+      secret_code = []
+
+      4.times { secret_code.push(rand(8) + 1) }
+      return secret_code
+
+    else
+      secret_code = false
+      until secret_code
+        Display.ask_user_secret_code
+        user_input = gets.chomp
+
+        if user_input.length == 4 && user_input.split('').all { |value| (value.to_i + 1).between(1, 8) }
+          secret_code = user_input
+
+        else
+          Display.ask_user_errorless_code
+        end
+      end
+    end
+  end
 end
