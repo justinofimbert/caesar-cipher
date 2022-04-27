e1c34151c4403851b2492659043b37bac2b2838f
justinofimbert <justinofimbert@gmail.com>
2022-04-27 17:09:56 -0300

    Create set_secret_code to Board class and add secret_code to attr_reader

--- mastermind/board.rb
+++ mastermind/board.rb
@@ -1,7 +1,11 @@
 class Board
-  attr_reader :max_guesses
+  attr_reader :max_guesses, :secret_code
 
   def initialize(max_guesses)
     @max_guesses = max_guesses
+  end
+
+  def set_secret_code=(secret_code)
+    @secret_code = secret_code
   end
 end
